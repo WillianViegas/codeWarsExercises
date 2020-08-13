abb2c9e36b01415f074beeabc3c718a909f2c815
Will <willianwed98@gmail.com>
2020-08-13 15:20:16 -0300

    DetectPangram and ShortestWord exercises

---
+++ codeWarsExercises/src/codeWarsExercises/DetectPangram.java
@@ -0,0 +1,42 @@
+package codeWarsExercises;
+
+public class DetectPangram {
+
+	public static void main(String[] args) {
+		System.out.println(check2("The quick brown fox jumps over the lazy dog"));
+	}
+	
+	 public static boolean check(String sentence){
+		    boolean[] mark = new boolean[26];
+		    
+		    int index = 0;
+		    
+		    for(int i = 0; i < sentence.length(); i++) {
+		    	if('A' <= sentence.charAt(i) && sentence.charAt(i) <= 'Z') {
+		    		index = sentence.charAt(i) - 'A';
+		    	}
+		    	else if('a' <= sentence.charAt(i) && sentence.charAt(i) <= 'z') {
+		    		index = sentence.charAt(i) - 'a';
+		    	}
+		    	else 
+		    		continue;
+		    		mark[index] = true;
+		    }
+		    
+		    for(int i = 0; i <= 25; i++) {
+		    	if(mark[i] == false) {
+		    		return false;
+		    	}
+		    }
+		    
+		    return true;
+		  }
+	 
+	//better way 
+	 public static boolean check2(String sentence){
+		    return sentence.chars()
+		    		.map(Character::toLowerCase)
+		    		.filter(Character::isAlphabetic).
+		    		distinct().count() == 26;
+		  }
+}
---
+++ codeWarsExercises/src/codeWarsExercises/ShortestWord.java
@@ -0,0 +1,34 @@
+package codeWarsExercises;
+
+import java.util.stream.Stream;
+
+public class ShortestWord {
+
+	public static void main(String[] args) {
+		System.out.println(findShort("turns out random test cases ar easier than writing out basic ones"));
+
+	}
+
+	  public static int findShort(String s) {
+		  String[] words = s.split(" ");
+		  int size = words.length; 
+		  int word;
+		  
+		  for(int i = 0; i < words.length; i++) {
+			  
+			  word = words[i].length();
+			  if(size > word) {
+				  size = word;
+			  }
+		  }
+		  return size;
+	    }
+	 
+	  //better way
+	  public static int findShort2(String s) {
+	        return Stream.of(s.split(" "))
+	          .mapToInt(String::length)
+	          .min()
+	          .getAsInt();
+	    }
+}
